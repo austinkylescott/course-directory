a0a11cdd3784c25ce92b929a8d6668ffd9ccc1ad
Austin Scott <austin.scott18@gmail.com>
2019-05-20 10:45:58 -0400

    Updates

---
+++ precache-manifest.597d1093d0cf58822dc6fb5658ce01de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a888b840c586dfd3957",
+    "url": "/course-directory/static/css/main.98e83402.chunk.css"
+  },
+  {
+    "revision": "7a888b840c586dfd3957",
+    "url": "/course-directory/static/js/main.7a888b84.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "c2641ee266d9d72c979e03b924ad79bf",
+    "url": "/course-directory/index.html"
+  }
+];
